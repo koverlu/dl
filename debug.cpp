f15d914591c1392c7b73ad68005f66b36a75a93a
Kover <remkko@163.com>
2017-12-14 11:19:44 +0800

    Update

--- debug.cpp
+++ debug.cpp
@@ -13,7 +13,9 @@ void DBG_ASSERT(bool ast, const char * format, ...)
 		_vsnprintf(buf, sizeof(buf), format, ap);
 		va_end(ap);
 		std::cout << buf << std::endl;
+#ifdef WIN_OS
 		OutputDebugString(buf);
+#endif
 		assert(0);
 	}
 }
@@ -26,5 +28,16 @@ void DBG_PRINT(const char * format, ...)
 	_vsnprintf(buf, sizeof(buf), format, ap);
 	va_end(ap);
 	std::cout << buf << std::flush;
+#ifdef WIN_OS
 	OutputDebugString(buf);
+#endif
 }
+
+#ifndef WIN_OS
+unsigned long GetTickCount()
+{
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	return (ts.tv_sec * 1000 + ts.tv_nsec / 1000000);  
+}
+#endif
--- debug.h
+++ debug.h
@@ -1,5 +1,12 @@
 #pragma once
+#ifdef WIN_OS
 #include <windows.h>
+#else
+#include <time.h>
+#define sprintf_s snprintf
+#define _vsnprintf vsnprintf 
+extern unsigned long GetTickCount();
+#endif
 
 extern void DBG_ASSERT(bool ast, const char * format, ...);
 extern void DBG_PRINT(const char * format, ...);
